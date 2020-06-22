03644bc80a4cf9ed0c5ce90441360d426fc2d6f8
Parrot User <user@localhost.localdomain>
2020-06-22 09:34:54 -0400

    changes name in init-env-vars

--- init-env-vars.sh
+++ init-env-vars.sh
@@ -4,6 +4,6 @@ set -e
 
 export LOCATION=useast2
 export COMMON_RESOURCE_GROUP_NAME=terraform-ref-rg
-export TF_STATE_STORAGE_ACCOUNT_NAME=tfstate2195
+export TF_STATE_STORAGE_ACCOUNT_NAME=tfstate-storage-account
 export TF_STATE_CONTAINER_NAME=tfstate-ref
 export KEYVAULT_NAME=terraform-ref-kv
