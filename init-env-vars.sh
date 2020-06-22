f8bc7ffaca7462973a27d3375ddd5153960875da
Parrot User <user@localhost.localdomain>
2020-06-22 08:45:43 -0400

    updating .gitignore

--- init-env-vars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export LOCATION=useast2
-export COMMON_RESOURCE_GROUP_NAME=terraform-ref-rg
-export TF_STATE_STORAGE_ACCOUNT_NAME=tfstate2195
-export TF_STATE_CONTAINER_NAME=tfstate-ref
-export KEYVAULT_NAME=terraform-ref-kv
