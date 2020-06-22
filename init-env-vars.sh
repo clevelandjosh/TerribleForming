e7d85737fe65e113fa6b0135d5a7f571e987302a
Parrot User <user@localhost.localdomain>
2020-06-22 11:20:34 -0400

    updated vars again, lowercase only

--- init-env-vars.sh
+++ init-env-vars.sh
@@ -4,6 +4,6 @@ set -e
 
 export LOCATION=eastus2
 export COMMON_RESOURCE_GROUP_NAME=terraform-ref-rg
-export TF_STATE_STORAGE_ACCOUNT_NAME=tfstate-store-acct
+export TF_STATE_STORAGE_ACCOUNT_NAME=tfstatestoreacct
 export TF_STATE_CONTAINER_NAME=tfstate-ref
 export KEYVAULT_NAME=terraform-ref-kv
